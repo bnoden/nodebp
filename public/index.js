0f359aeff6f0ca4fbf02366052abdab6dc554cf8
Alexander Solinger <alexandersolinger@me.com>
2023-10-19 13:29:03 -0700

    UPDATE: adds get request functionality

--- public/index.js
+++ public/index.js
@@ -1,92 +1,115 @@
 
+let characterData = [];
 
-const characterData = [
-  {
-      "id": 1,
-      "name": "Mario",
-      "quotes": [
-          {
-              "id": 1,
-              "quote": "It's a me, Mario!"
-          },
-          {
-              "id": 2,
-              "quote": "Let's-a-go!"
-          },
-          {
-              "id": 3,
-              "quote": "Yipeeeeeee!"
-          }
-      ]
+fetch('http://localhost:8080/persona/characters', {
+  method: 'GET', // This is the default method, so you can omit it.
+  headers: {
+    'Content-Type': 'application/json', // Specify the content type if needed.
+    // You can add more headers here if required.
   },
-  {
-      "id": 2,
-      "name": "John McLane",
-      "quotes": [
-          {
-              "id": 4,
-              "quote": "Yipee-kaye, Mother Fucker!"
-          },
-          {
-              "id": 5,
-              "quote": "Awww, no bullets!"
-          },
-          {
-              "id": 6,
-              "quote": "Come out to the coast, we'll get together, have a few laughs..."
-          }
-      ]
-  },
-  {
-      "id": 3,
-      "name": "Arnold Schwarzenegger",
-      "quotes": [
-          {
-              "id": 7,
-              "quote": "I'll be back."
-          },
-          {
-              "id": 8,
-              "quote": "Hasta la vista, baby."
-          },
-          {
-              "id": 9,
-              "quote": "It's not a tumor!"
-          }
-      ]
-  }
-];
+  // You can include other options like credentials, mode, etc.
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the response as JSON (or use .text() for plain text).
+  })
+  .then(data => {
+    console.log(data); // Do something with the JSON data.
+    characterData = data;
+    let currentCharacter = characterData[0];
+    console.log(currentCharacter);
+    let currentQuoteList = currentCharacter.quotes;
+    let characterQuote = document.getElementById("character-quote");
+    const quoteButton = document.getElementById("quote-button");
+    const characterDropDown = document.getElementById('characterDropDown');
 
-let currentCharacter = characterData[0];
-let currentQuoteList = currentCharacter.quotes;
-let characterQuote = document.getElementById("character-quote");
-const quoteButton = document.getElementById("quote-button");
-const characterDropDown = document.getElementById('characterDropDown');
+    for (let i in characterData) {
+      let option = document.createElement("option");
 
-for (let i in characterData) {
-  let option = document.createElement("option");
+      option.setAttribute('value', characterData[i].id);
 
-  option.setAttribute('value', characterData[i].id);
+      let optionText = document.createTextNode(characterData[i].name);
 
-  let optionText = document.createTextNode(characterData[i].name);
+      option.appendChild(optionText);
 
-  option.appendChild(optionText);
+      characterDropDown.appendChild(option);
+    }
 
-  characterDropDown.appendChild(option);
-}
+    characterDropDown.addEventListener('change', e => {
+      let currentCharacterId = e.target.value;
+      currentCharacter = characterData[currentCharacterId - 1];
+      document.getElementById('character-quote').innerHTML = "";
+      currentQuoteList = currentCharacter.quotes;
+      console.log(currentCharacter);
+    });
 
-characterDropDown.addEventListener('change', e => {
-  let currentCharacterId = e.target.value;
-  currentCharacter = characterData[currentCharacterId - 1];
-  document.getElementById('character-quote').innerHTML = "";
-  currentQuoteList = currentCharacter.quotes;
-  console.log(currentCharacter);
-});
-
-quoteButton.addEventListener("click", e =>{
-  console.log(currentQuoteList);
-  characterQuote.innerHTML = currentQuoteList[Math.floor(Math.random() * currentQuoteList.length)].quote;
-});
+    quoteButton.addEventListener("click", e =>{
+      console.log(currentQuoteList);
+      characterQuote.innerHTML = currentQuoteList[Math.floor(Math.random() * currentQuoteList.length)].quote;
+    });
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+  
+// const characterData = [
+//   {
+//       "id": 1,
+//       "name": "Mario",
+//       "quotes": [
+//           {
+//               "id": 1,
+//               "quote": "It's a me, Mario!"
+//           },
+//           {
+//               "id": 2,
+//               "quote": "Let's-a-go!"
+//           },
+//           {
+//               "id": 3,
+//               "quote": "Yipeeeeeee!"
+//           }
+//       ]
+//   },
+//   {
+//       "id": 2,
+//       "name": "John McLane",
+//       "quotes": [
+//           {
+//               "id": 4,
+//               "quote": "Yipee-kaye, Mother Fucker!"
+//           },
+//           {
+//               "id": 5,
+//               "quote": "Awww, no bullets!"
+//           },
+//           {
+//               "id": 6,
+//               "quote": "Come out to the coast, we'll get together, have a few laughs..."
+//           }
+//       ]
+//   },
+//   {
+//       "id": 3,
+//       "name": "Arnold Schwarzenegger",
+//       "quotes": [
+//           {
+//               "id": 7,
+//               "quote": "I'll be back."
+//           },
+//           {
+//               "id": 8,
+//               "quote": "Hasta la vista, baby."
+//           },
+//           {
+//               "id": 9,
+//               "quote": "It's not a tumor!"
+//           }
+//       ]
+//   }
+// ];
 
 
 /* Brandon's old code */
