f8f2dda07acc56439aa2c167ae77c116e4f0e816
Alexander Solinger <alexandersolinger@me.com>
2023-10-19 13:02:05 -0700

    UPDATE: adds dropdown functionality and speak button functionality

--- public/index.js
+++ public/index.js
@@ -1,99 +1,215 @@
+
+
+const characterData = [
+  {
+      "id": 1,
+      "name": "Mario",
+      "quotes": [
+          {
+              "id": 1,
+              "quote": "It's a me, Mario!"
+          },
+          {
+              "id": 2,
+              "quote": "Let's-a-go!"
+          },
+          {
+              "id": 3,
+              "quote": "Yipeeeeeee!"
+          }
+      ]
+  },
+  {
+      "id": 2,
+      "name": "John McLane",
+      "quotes": [
+          {
+              "id": 4,
+              "quote": "Yipee-kaye, Mother Fucker!"
+          },
+          {
+              "id": 5,
+              "quote": "Awww, no bullets!"
+          },
+          {
+              "id": 6,
+              "quote": "Come out to the coast, we'll get together, have a few laughs..."
+          }
+      ]
+  },
+  {
+      "id": 3,
+      "name": "Arnold Schwarzenegger",
+      "quotes": [
+          {
+              "id": 7,
+              "quote": "I'll be back."
+          },
+          {
+              "id": 8,
+              "quote": "Hasta la vista, baby."
+          },
+          {
+              "id": 9,
+              "quote": "It's not a tumor!"
+          }
+      ]
+  }
+];
+
+let currentCharacter = characterData[0];
+let currentQuoteList = currentCharacter.quotes;
+let characterQuote = document.getElementById("character-quote");
+const quoteButton = document.getElementById("quote-button");
+const characterDropDown = document.getElementById('characterDropDown');
+
+for (let i in characterData) {
+  let option = document.createElement("option");
+
+  option.setAttribute('value', characterData[i].id);
+
+  let optionText = document.createTextNode(characterData[i].name);
+
+  option.appendChild(optionText);
+
+  characterDropDown.appendChild(option);
+}
+
+characterDropDown.addEventListener('change', e => {
+  let currentCharacterId = e.target.value;
+  currentCharacter = characterData[currentCharacterId - 1];
+  document.getElementById('character-quote').innerHTML = "";
+  currentQuoteList = currentCharacter.quotes;
+  console.log(currentCharacter);
+});
+
+quoteButton.addEventListener("click", e =>{
+  console.log(currentQuoteList);
+  characterQuote.innerHTML = currentQuoteList[Math.floor(Math.random() * currentQuoteList.length)].quote;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Brandon's old code */
+
 // const getAll = require('./components/GetAll');
 
-const characterForm = document.getElementById('characterForm');
-const selectionInput = document.getElementById('selectionInput');
-const btnSelect = document.getElementById('btnSelect');
-const selection = document.getElementById('selection');
-const character = document.getElementById('character');
-const quoteBox = document.querySelector('.quote-container');
+// const characterForm = document.getElementById('characterForm');
+// const selectionInput = document.getElementById('selectionInput');
+// const btnSelect = document.getElementById('btnSelect');
+// const selection = document.getElementById('selection');
+// const character = document.getElementById('character');
+// const quoteBox = document.querySelector('.quote-container');
 
-let currentCharacter = {};
-let characterList = [];
-let data = [];
-let NUMBER_OF_CHARACTERS;
+// const charactersDropDown = document.getElementById('charactersDropDown');
 
-((url = './db/all.json', cb) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.responseType = 'json';
+// let currentCharacter = {};
+// let characterList = [];
+// let data = [];
+// let NUMBER_OF_CHARACTERS;
 
-  request.onload = () => {
-    data = request.response;
-    NUMBER_OF_CHARACTERS = data.length;
+// ((url = './db/all.json', cb) => {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', url);
+//   request.responseType = 'json';
 
-    data.map((i) => {
-      characterList = characterList.concat(i.name);
-      selectionInput.innerHTML += `<option value="${i.name}" class="persona-${i.id}" data-id="${i.id}" />`;
-    });
+//   request.onload = () => {
+//     data = request.response;
+//     NUMBER_OF_CHARACTERS = data.length;
 
-    console.log('characterList', characterList);
+//     data.map((i) => {
+//       characterList = characterList.concat(i.name);
+//       selectionInput.innerHTML += `<option value="${i.name}" class="persona-${i.id}" data-id="${i.id}" />`;
+//     });
 
-    selectionInput.focus();
-  };
+//     console.log('characterList', characterList);
 
-  request.send();
+//     selectionInput.focus();
+//   };
 
-  let quoteIndex = -1;
-  const getQuoteIndex = () => quoteIndex;
-  const setQuoteIndex = (int) => (quoteIndex = int);
+//   request.send();
 
-  btnSelect.onclick = function () {
-    let quoteList = currentCharacter.quotes;
-    let index = getQuoteIndex();
-    if (quoteList.length > 0) {
-      quoteBox.innerHTML = quoteList[index];
-    }
-  };
+//   let quoteIndex = -1;
+//   const getQuoteIndex = () => quoteIndex;
+//   const setQuoteIndex = (int) => (quoteIndex = int);
 
-  const getSelectedId = () =>
-    ~~selectionInput.children[selectionInput.selectedIndex].getAttribute(
-      'data-id'
-    );
+//   btnSelect.onclick = function () {
+//     let quoteList = currentCharacter.quotes;
+//     let index = getQuoteIndex();
+//     if (quoteList.length > 0) {
+//       quoteBox.innerHTML = quoteList[index];
+//     }
+//   };
 
-  function setCurrentCharacter() {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === getSelectedId()) {
-        currentCharacter = {
-          id: data[i].id,
-          name: data[i].name,
-          quotes: data[i].quotes.map((quote) => quote),
-        };
-      }
-    }
-  }
+//   const getSelectedId = () =>
+//     ~~selectionInput.children[selectionInput.selectedIndex].getAttribute(
+//       'data-id'
+//     );
 
-  selectionInput.onchange = function () {
-    setCurrentCharacter();
-    //characterForm.reset();
-  };
+//   function setCurrentCharacter() {
+//     for (let i = 0; i < data.length; i++) {
+//       if (data[i].id === getSelectedId()) {
+//         currentCharacter = {
+//           id: data[i].id,
+//           name: data[i].name,
+//           quotes: data[i].quotes.map((quote) => quote),
+//         };
+//       }
+//     }
+//   }
 
-  characterForm.addEventListener('keydown', (event) => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-    }
-  });
+//   selectionInput.onchange = function () {
+//     setCurrentCharacter();
+//     //characterForm.reset();
+//   };
 
-  btnSelect.addEventListener('click', (event) => {
-    event.preventDefault();
-  });
+//   characterForm.addEventListener('keydown', (event) => {
+//     if (event.keyCode === 13) {
+//       event.preventDefault();
+//     }
+//   });
 
-  btnSelect.onmousedown = (event) => {
-    btnSelect.style.color = '#139C63';
-    btnSelect.style.backgroundColor = '#E3FFF1';
-    btnSelect.style.borderColor = '#139C63';
-    event.preventDefault();
-  };
+//   btnSelect.addEventListener('click', (event) => {
+//     event.preventDefault();
+//   });
 
-  btnSelect.onmouseup = (event) => {
-    btnSelect.style.color = '#30c9a3';
-    btnSelect.style.backgroundColor = '#fafcfb';
-    btnSelect.style.borderColor = '#30c9a3';
-    event.preventDefault();
-  };
+//   btnSelect.onmousedown = (event) => {
+//     btnSelect.style.color = '#139C63';
+//     btnSelect.style.backgroundColor = '#E3FFF1';
+//     btnSelect.style.borderColor = '#139C63';
+//     event.preventDefault();
+//   };
 
-  btnSelect.onmouseleave = () => {
-    event.preventDefault();
-  };
+//   btnSelect.onmouseup = (event) => {
+//     btnSelect.style.color = '#30c9a3';
+//     btnSelect.style.backgroundColor = '#fafcfb';
+//     btnSelect.style.borderColor = '#30c9a3';
+//     event.preventDefault();
+//   };
 
-  btnSelect.onmouseover = () => {};
-})();
+//   btnSelect.onmouseleave = () => {
+//     event.preventDefault();
+//   };
+
+//   btnSelect.onmouseover = () => {};
+// })();
